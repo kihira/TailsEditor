3653c422d5a9f68b7d19a60001c8aab6b5558ac2
Zoe Lee <zoe@kihira.uk>
2017-08-11 18:52:59 +0100

    Initial framework for selecting objects

--- src/program.ts
+++ src/program.ts
@@ -1,26 +1,17 @@
 import * as THREE from 'three'
 
-let scene = new THREE.Scene();
-let camera = new THREE.Camera();
-let renderer = new THREE.WebGLRenderer();
+const scene = new THREE.Scene();
+const camera = new THREE.Camera();
+const renderer = new THREE.WebGLRenderer();
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+let selectedObject: THREE.Object3D;
+let selectedAxisHelper = new THREE.AxisHelper();
 
-let axis = new THREE.AxisHelper(10);
+/** Functions **/
 
-function createPlayer() {
-/*    var loader = new THREE.TextureLoader();
-    loader.load('textures/player.png',
-        function (texture) {
-            var material = new THREE.MeshBasicMaterial({
-                map: texture
-            });
-        }, function (progress) {
-            console.log((progress.loaded / progress.total * 100) + '% loaded');
-        }, function (err) {
-            console.error(err.message);
-        });*/
+function loadPlayerModel() {
     let modelLoader = new THREE.JSONLoader();
     modelLoader.load('models/player.png',
         function (geometry, materials) {
@@ -34,3 +25,44 @@ function createPlayer() {
             console.error(err.message);
         });
 }
+
+function setSelectedObject(obj: THREE.Object3D) {
+    if (selectedObject != null) {
+        selectedAxisHelper.visible = false;
+        // todo remove highlight
+    }
+
+    if (obj == null) return;
+
+    selectedObject = obj;
+    selectedAxisHelper.visible = true;
+    selectedAxisHelper.setRotationFromEuler(selectedObject.getWorldRotation());
+    // todo highlight selected object
+}
+
+function render() {
+    renderer.render(scene, camera);
+}
+
+function onMouseMove(event: MouseEvent) {
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}
+
+function onMouseClick(event: MouseEvent) {
+    raycaster.setFromCamera(mouse, camera);
+
+    let intersects = raycaster.intersectObjects(scene.children, true); // todo ignore player model
+    if (intersects.length == 0) setSelectedObject(null);
+
+    selectedObject = intersects[0].object;
+}
+
+/** Run code **/
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+window.requestAnimationFrame(render);
+renderer.domElement.addEventListener('mousemove', onMouseMove, false);
+renderer.domElement.addEventListener('mousedown', onMouseClick, false);
