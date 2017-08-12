3142afaa4be68ff02e0f2043ea31e9cfc5bb697d
Zoe Lee <zoe@kihira.uk>
2017-08-12 18:27:43 +0100

    Fix building typescript for browser by using gulp and browserify

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+export = 0;
+
 import * as THREE from 'three'
 
 const scene = new THREE.Scene();
@@ -6,6 +8,7 @@ const renderer = new THREE.WebGLRenderer();
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
 
+let playerObject: THREE.Object3D;
 let selectedObject: THREE.Object3D;
 let selectedAxisHelper = new THREE.AxisHelper();
 
@@ -13,16 +16,16 @@ let selectedAxisHelper = new THREE.AxisHelper();
 
 function loadPlayerModel() {
     let modelLoader = new THREE.JSONLoader();
-    modelLoader.load('models/player.png',
+    modelLoader.load('models/player.json',
         function (geometry, materials) {
             let material = materials[0];
-            let object = new THREE.Mesh(geometry, material);
+            playerObject = new THREE.Mesh(geometry, material);
 
-            scene.add(object);
+            scene.add(playerObject);
         }, function (progress) {
             console.log((progress.loaded / progress.total * 100) + '% loaded');
         }, function (err) {
-            console.error(err.message);
+            console.error(err);
         });
 }
 
@@ -60,9 +63,36 @@ function onMouseClick(event: MouseEvent) {
 
 /** Run code **/
 
+renderer.domElement.addEventListener('mousemove', onMouseMove, false);
+renderer.domElement.addEventListener('mousedown', onMouseClick, false);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 window.requestAnimationFrame(render);
-renderer.domElement.addEventListener('mousemove', onMouseMove, false);
-renderer.domElement.addEventListener('mousedown', onMouseClick, false);
+
+// Load default models
+loadPlayerModel();
+
+// Create base grid
+let geometry = new THREE.BufferGeometry();
+let vertices = new Float32Array(20 * 2 * 3);
+let index = 0;
+for (let x = -10; x <= 10; x++) {
+    vertices[index] = x;
+    vertices[index+1] = -10;
+    vertices[index+2] = 0;
+    vertices[index+3] = x;
+    vertices[index+4] = 10;
+    vertices[index+5] = 0;
+    index += 6;
+}
+let grid = new THREE.LineSegments(geometry, new THREE.LineBasicMaterial({
+    color: 0xffffff,
+    linewidth: 1,
+    linejoin: "round",
+    linecap: "round"
+
+}));
+scene.add(grid);
+
+render();
